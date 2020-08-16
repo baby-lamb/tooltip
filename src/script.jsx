71cf72f4b72653a74ea7b8fecf74d9d1b2422c97
Pam <59804940+Naju-Baek@users.noreply.github.com>
2020-08-16 23:37:08 +0900

    추가

---
+++ src/script.jsx
@@ -0,0 +1,22 @@
+
+class Tooltip extends React.Component{
+  constructior(props){
+
+  }
+  toggle(){
+
+  }
+  render(){
+
+  }
+
+}
+
+ReactDOM.render(
+  <div>
+    <Tooltip text="The book you're reading now">React Quickly</Tooltip>
+    was published in 2020. It's awesome!
+  </div>,
+  document.getElementById('tooltip')
+
+)
